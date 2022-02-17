a485df6df81a61c1e2c14f65b486e3d083629263
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2022-02-16 23:07:35 -0800

    beofre change

--- stacklang.hs
+++ stacklang.hs
@@ -12,6 +12,7 @@ type Prog = [Cmd]
 -- The stack supports Bools and Ints
 type Stack = [Val]
 data Val =  I Int | B Bool
+  deriving Show
 
 -- These are the commands supported
 data Cmd  = LDI Int
@@ -33,6 +34,11 @@ type Rank  = Int
 -- to add and m = ele to remove
 type CmdRank = (Int, Int)
 
+-- Final return type, supports error
+data Final = RankError | TypeError | A Stack
+  deriving Show
+
+
 -- Defines the rank of each supported command
 rankC :: Cmd -> CmdRank
 rankC (LDI a) = (0, 1)
@@ -46,14 +52,17 @@ rankC SWAP = (2, 1)
 rankC (POP k) = (k, 0)
 rankC (IFELSE prog1 prog2) = addCmdRank (largerRank(addAllRanks prog1) (addAllRanks prog2)) (1,0)
 
+
 -- Helper function - adds two CmdRanks
 addCmdRank :: CmdRank -> CmdRank -> CmdRank
 addCmdRank (a, b) (c, d) = (a + c, b + d)
+
 
 -- Helper function - adds an array of [Cmd] into one CmdRank
 addAllRanks :: Prog -> CmdRank
 addAllRanks (x:xs) = addCmdRank (rankC x) (addAllRanks xs)
 addAllRanks [] = (0, 0)
+
 
 -- Helper function - determines the larger of two CmdRanks
 largerRank :: CmdRank -> CmdRank -> CmdRank
@@ -62,8 +71,8 @@ largerRank (a, b) (c, d)
  | otherwise = (a, b)
 
 
-
--- rankP 
+-- rankP - Determines the resulting rank of a program
+-- on a stack, or if there would be a RankError
 rankP :: Prog -> Rank -> Maybe Rank
 rankP prog i
   | rank prog i < 0 = Nothing -- accounts for RankError
@@ -72,26 +81,47 @@ rankP prog i
 rank :: Prog -> Rank -> Rank
 rank prog i = (i - fst (addAllRanks prog))
 
-
-a :: Prog
-a = [ADD, IFELSE [ADD] [ADD, ADD]]
-
-
+-- evalStatTC - Checks if a program is rank valid,
+-- otherwise produces a RankError
+evalStatTC :: Prog -> Stack -> Maybe Stack
+evalStatTC prog s 
+  | rankP prog (length s) /= Nothing = semCmd prog s
+  | otherwise = Nothing
 
 
 -- Semantic definition of how to run a program
 -- Run commands on a stack until no commands are left
 semCmd :: Prog -> Stack -> Maybe Stack
-semCmd ((LDI a):ps) s = semCmd ps (Right a:s) -- Append a on the stack
-semCmd ((LDB a):ps) s = semCmd ps (Left a:s) -- Same as above, but a Left bool type
-semCmd (ADD:ps) ((Right x):(Right y):xs) = semCmd ps (Right (x + y):xs) -- Add with INTs only
-semCmd (MULT:ps) ((Right x):(Right y):xs) = semCmd ps (Right (x * y):xs) -- Mult with INTs only
+semCmd ((LDI a):ps) s = semCmd ps (I a:s) -- Append a on the stack
+semCmd ((LDB a):ps) s = semCmd ps (B a:s) -- Same as above, but a Left bool type
+semCmd (ADD:ps) ((I x):(I y):xs) = semCmd ps ((I (x + y)):xs) -- Add with INTs only
+semCmd (MULT:ps) ((I x):(I y):xs) = semCmd ps ((I (x * y)):xs) -- Mult with INTs only
 semCmd (DUP:ps) (x:xs) = semCmd ps (x:x:xs) -- Dupe with either type, just con twice
-semCmd (LEQ:ps) (x:y:xs) -- Append a Left True/False depending on condition
-  | x <= y = semCmd ps (Left True:xs)
-  | otherwise = semCmd ps (Left False:xs)
-semCmd ((IFELSE prog1 prog2):ps) (Left x:xs) -- Append the program instructions, depending on condition
+semCmd (LEQ:ps) (I x:I y:xs) -- Append a Left True/False depending on condition
+  | x <= y = semCmd ps (B True:xs)
+  | otherwise = semCmd ps (B False:xs)
+semCmd ((IFELSE prog1 prog2):ps) (B x:xs) -- Append the program instructions, depending on condition
   | x = semCmd (prog1 ++ ps) xs
   | otherwise = semCmd (prog2 ++ ps) xs
+semCmd (DEC:ps) (I x:xs) = semCmd ps ((I (x-1)):xs) -- Decrements the top element
+semCmd (SWAP:ps) (x:y:xs) = semCmd ps (y:x:xs) -- Swap the top two elements on the stack
+semCmd ((POP k):ps) s = semCmd ps (drop k s)
 semCmd [] s = Just s -- Once out of commands, return the stack
 semCmd _ _ = Nothing -- If it does not pattern match, it is an invalid command
+
+-- run->stemstattc(check for rankerror)->semcmd (check for type error)
+run :: Prog -> Stack -> Final
+run prog s = semStatTC prog s
+
+
+
+-- Test Cases:
+a :: Prog
+a = [LDB False, IFELSE [LDI 1] [LDI 2]]
+b :: Prog
+b =[ADD]
+
+s :: Stack
+s = [I 5, I 5, B True, I 2, I 3]
+p :: Stack
+p = [I 1]
